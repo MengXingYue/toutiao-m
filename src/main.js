6a29444f4b5135b2241cd7114aca1b8db854a214
冰糖橙 <12777479+rock-sugar-orange@user.noreply.gitee.com>
2023-04-23 16:57:28 +0800

    引入Vant组件库

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import App from './App.vue' // 加载跟组件
+import router from './router' // 加载路由
 import store from './store'
+import Vant from 'vant' /// /加载vant
+import 'vant/lib/index.css' // 加载vant的全局样式
 
-// 加载全局样式
-import './styles/index.less'
+import './styles/index.less'// 加载自己的全局样式
+
+Vue.use(Vant) // 注册使用Vant组件库
 
 Vue.config.productionTip = false
 
